16410aa3355f8d910400aa9b47b16e06860dfa4c
Sean-m99 <2317103m@student.gla.ac.uk>
2025-03-12 19:31:45 +0000

    Update pageflip-query.js
    Added an alert that comes up when somebody tries to leave the page

--- static/js/pageflip-query.js
+++ static/js/pageflip-query.js
@@ -1,3 +1,4 @@
-$(document).ready(function() {
-    alert('Hello, world!');
+window.addEventListener('beforeunload', function (event) {
+    event.preventDefault();
+    event.returnValue = 'Are you sure you want to leave this page? Plenty of books await.'; 
 });
