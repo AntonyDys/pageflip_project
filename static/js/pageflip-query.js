858c258401c9cc94a43a8339282e1b97a0ed2b77
Sean-m99 <2317103m@student.gla.ac.uk>
2025-03-13 10:04:25 +0000

    Update pageflip-query.js

--- static/js/pageflip-query.js
+++ static/js/pageflip-query.js
@@ -3,14 +3,4 @@ window.addEventListener('beforeunload', function (event) {
     event.returnValue = 'Are you sure you want to leave this page? Plenty of books await.'; 
 });
 
-//highlighting stuff when the cursor is over it - don't know if this works
 
-document.querySelectorAll('.highlightable').forEach(function(element) {
-    element.addEventListener('mouseover', function() {
-        element.style.backgroundColor = 'purple'; 
-    });
-    
-    element.addEventListener('mouseout', function() {
-        element.style.backgroundColor = ''; 
-    });
-});
