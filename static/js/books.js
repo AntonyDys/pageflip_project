0f22f83627e5598f0b65a3dc2ef445acc4e7e270
Antony Dyson <3067791D@student.gla.ac.uk>
2025-03-16 00:32:14 +0000

    Moved js out of line for books.html

---
+++ static/js/books.js
@@ -0,0 +1,8 @@
+function toggleSubgenres() {
+            var menu = document.getElementById("subgenre-menu");
+            if (menu.style.display === "none") {
+                menu.style.display = "block";
+            } else {
+                menu.style.display = "none";
+            }
+        }
