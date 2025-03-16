cd0ddca5de59ede33556f3111b64146d333a467b
Sean-m99 <2317103m@student.gla.ac.uk>
2025-03-16 12:12:04 +0000

    Update pageflip-query.js

--- static/js/pageflip-query.js
+++ static/js/pageflip-query.js
@@ -3,4 +3,12 @@ window.addEventListener('beforeunload', function (event) {
     event.returnValue = 'Are you sure you want to leave this page? Plenty of books await.'; 
 });
 
-
+    document.querySelectorAll(".book-card").forEach(card => {
+        card.addEventListener("mouseenter", function () {
+            this.style.transform = "scale(1.05)";
+            this.style.transition = "transform 0.3s ease-in-out";
+        });
+        card.addEventListener("mouseleave", function () {
+            this.style.transform = "scale(1)";
+        });
+    });
