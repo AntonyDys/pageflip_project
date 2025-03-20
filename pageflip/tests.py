332e0fb5451ed1d553a80bf535bd5ae0ae4751b7
Antony Dyson <3067791d@student.gla.ac.uk>
2025-03-20 10:28:28 +0000

    Tests

--- pageflip/tests.py
+++ pageflip/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+#testing that the index notifies when no books are in the most popular
+class IndexTest(TestCase):
+    def test_index_no_books(self):
+        response = self.client.get(reverse('pageflip:index'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No books available.')
+        self.assertQuerysetEqual(response.context['books'], [])
+
+#testing when there are no books in the books page
+class BookTest(TestCase):
+    def test_book_no_books(self):
+        response = self.client.get(reverse('pageflip:books'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No books available.')
+        self.assertQuerysetEqual(response.context['books'], [])
